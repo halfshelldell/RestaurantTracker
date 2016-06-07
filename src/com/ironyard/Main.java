b2c0306a24c0dfdef87ed2d186836de36707d00b
halfshelldell <simmonslydell@gmai.com>
2016-06-07 11:01:56 -0400

    added username, password + login button

--- src/com/ironyard/Main.java
+++ src/com/ironyard/Main.java
@@ -1,8 +1,25 @@
 package com.ironyard;
 
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.HashMap;
+
 public class Main {
 
+    static HashMap<String, User> users = new HashMap<>();
+
     public static void main(String[] args) {
-	// write your code here
+        Spark.init();
+        Spark.get(
+                "/",
+                (request, response) -> {
+                    HashMap m = new HashMap();
+                    return new ModelAndView(m, "login.html");
+                },
+                new MustacheTemplateEngine()
+        );
+
     }
 }
---
+++ src/com/ironyard/Restaurant.java
@@ -0,0 +1,18 @@
+package com.ironyard;
+
+/**
+ * Created by illladell on 6/7/16.
+ */
+public class Restaurant {
+    String name;
+    String location;
+    int rating;
+    String comment;
+
+    public Restaurant(String name, String location, int rating, String comment) {
+        this.name = name;
+        this.location = location;
+        this.rating = rating;
+        this.comment = comment;
+    }
+}
