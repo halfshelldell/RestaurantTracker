712e7f4d45da0a1868966306582e23acc5e481c9
halfshelldell <simmonslydell@gmai.com>
2016-06-07 11:46:08 -0400

    add logout and form actions

--- src/com/ironyard/Main.java
+++ src/com/ironyard/Main.java
@@ -1,6 +1,7 @@
 package com.ironyard;
 
 import spark.ModelAndView;
+import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
@@ -15,10 +16,83 @@ public class Main {
         Spark.get(
                 "/",
                 (request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
+
                     HashMap m = new HashMap();
-                    return new ModelAndView(m, "login.html");
+                    if (username == null) {
+                        return new ModelAndView(m, "login.html");
+                    }
+                    else {
+                        User user = users.get(username);
+                        m.put("restaurants", user.restaurants);
+                        return new ModelAndView(m, "home.html");
+                    }
                 },
                 new MustacheTemplateEngine()
+        );
+        Spark.post(
+                "/login",
+                (request, response) -> {
+                    String name = request.queryParams("username");
+                    String pass = request.queryParams("password");
+                    if (name == null || pass == null) {
+                        throw new Exception("Name or pass not sent");
+                    }
+
+                    User user = users.get(name);
+                    if (user == null) {
+                        user = new User(name, pass);
+                        users.put(name, user);
+                    }
+                    else if (!name.equals(user.name)) {
+                        throw new Exception("Wrong password");
+                    }
+
+                    Session session = request.session();
+                    session.attribute("username", name);
+
+                    response.redirect("/");
+                    return "";
+                }
+        );
+        Spark.post(
+                "/create-restaurant",
+                (request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
+                    if (username == null) {
+                        throw new Exception("Not logged in");
+                    }
+
+                    String name = request.queryParams("name");
+                    String location = request.queryParams("location");
+                    int rating = Integer.valueOf(request.queryParams("rating"));
+                    String comment = request.queryParams("comment");
+                    if (name == null || location == null || comment == null) {
+                        throw new Exception("Invalid form fields");
+                    }
+
+                    User user = users.get(username);
+                    if (user == null) {
+                        throw new Exception("User does not exist");
+                    }
+
+                    Restaurant r = new Restaurant(name, location, rating, comment);
+                    user.restaurants.add(r);
+
+                    response.redirect("/");
+                    return "";
+                }
+        );
+        Spark.post(
+                "/logout",
+                (request, response) -> {
+                    Session session = request.session();
+                    session.invalidate();
+                    response.redirect("/");
+                    return "";
+                }
         );
 
     }
